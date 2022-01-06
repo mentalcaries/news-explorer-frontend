436826e7779490e67f2f0d059062d369d9f7f1eb
Devin Jaggernauth <neexis@gmail.com>
2022-01-06 12:29:44 -0400

    make articles and results responsive

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import NewsCard from "../NewsCard/NewsCard";
 import './ResultCard.css'
 
 const ResultCard = () =>{
+
+  const [isSaved, setIsSaved] = useState(false)
+
   return(
   <NewsCard>
-      <button className="save-button" />
+      <button className={`save-button ${isSaved? 'save-button_active' :''}`} onClick={()=>{setIsSaved(!isSaved)}}/>
 </NewsCard>
   )}
 
