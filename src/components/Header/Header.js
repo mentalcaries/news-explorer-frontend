ea28bdb4a5c3bca1e5738817d674589723c5d034
Devin Jaggernauth <neexis@gmail.com>
2022-01-06 23:54:43 -0400

    add hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { useLocation } from 'react-router';
+import React, {useState} from 'react';
+import {useLocation} from 'react-router';
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
-function Header({onLogin, loggedIn }) {
-
-  const { pathname } = useLocation();
+function Header({onLogin, loggedIn}) {
+  const {pathname} = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [darkMode, setDarkMode] = useState(pathname==='/articles')
+  const lightHeader = pathname==='/articles'
   
   //conditional NavBar
+  //navbar open, menu icon === X
   return (
     <header className="header">
-      <div className="header__content">
-          <h2 className={`header__logo ${pathname==='/articles'? 'header__logo_light' :''}`}>NewsExplorer</h2>
-        <Navigation onLogin={onLogin} loggedIn={loggedIn}/>
+      <div
+        className={`header__content ${
+          lightHeader
+            ? 'header__content_light'
+            : ''
+        }`}
+      >
+        <h2
+          className={`header__logo ${
+           lightHeader? 'header__logo_light' : ''
+          }`}
+        >
+          NewsExplorer
+        </h2>
+        <button
+          className={`header__menu ${lightHeader? 'header__menu_light':''} ${isMenuOpen? 'header__menu_open':''} ${(isMenuOpen && lightHeader)?'header__menu_open_light':''}`}
+          onClick={() => {
+            setIsMenuOpen(!isMenuOpen);
+          }}
+        />
+        {isMenuOpen && <Navigation onLogin={onLogin} loggedIn={loggedIn} />}
       </div>
     </header>
   );
