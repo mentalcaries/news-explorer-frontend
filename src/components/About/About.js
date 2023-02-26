ce255854a5012dbd697e18530b6ee41215bb7c98
Devin Jaggernauth <neexis@gmail.com>
2023-02-26 00:12:08 -0400

    update About description

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 import profile from '../../images/dev.jpg';
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
@@ -11,13 +12,11 @@ function About() {
       <div className="about__text">
         <h2 className="about__title">About the Author</h2>
         <p className="about__paragraph">
-          Devin Jaggernauth is a Full Stack Developer who's currently working
-          with JavaScript and the MERN Stack having graduated from Practicum's
-          Web Dev program in January 2022.
+          Devin Jaggernauth is a Full Stack Developer who's experienced
+          with JavaScript/TypeScript and the MERN Stack. 
         </p>
         <p className="about__paragraph">
-          He's participated in real world projects such as DotCot and every code
-          jam since enrollment. He enjoys finding creative and efficient ways to
+          He's participated in real world projects such as DotCot, WTWR and is currently a mentor at <Link to={{  pathname: "https://the-collab-lab.codes/" }} target="_blank" >The Collab Lab</Link>. He enjoys finding creative and efficient ways to
           solve problems whether working on a small solo project, or with a
           team.
         </p>
